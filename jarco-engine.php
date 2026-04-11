acd9cda0415ac87e73b49250c5ddd47729e2efbb
NellFotina <fotina.nel@gmail.com>
2026-04-11 12:22:51 +0200

    изменение версии

--- jarco-engine.php
+++ jarco-engine.php
@@ -3,7 +3,7 @@
 Plugin Name: Jarco Engine
 Description: Управление продуктами Jarco с интеграцией WooCommerce (только вариативные товары)
 Author: Nelia Fotina
-Version: 1.4
+Version: 1.5
 Requires PHP: 7.4
 Text Domain: jarco-engine
 Domain Path: /languages
